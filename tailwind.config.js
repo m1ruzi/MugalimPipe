5ce3c5c6fd52fbb28b4539e1771357ebffd644f1
m1ruzi <164134403+m1ruzi@users.noreply.github.com>
2026-01-06 02:58:54 +0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // Ваши основные цвета
+        'black': '#000000',
+        'white': '#ffffff',
+        'dark-gray': '#1a1a1a', // Для мягкого фона или карточек
+        'light-gray': '#e5e5e5',
+      },
+      letterSpacing: {
+        'ultra-tight': '-0.05em', // Для стильных ч/б заголовков
+      }
+    },
   },
   plugins: [],
 };
